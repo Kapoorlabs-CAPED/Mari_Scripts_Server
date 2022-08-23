52092a3b0defa404718cc1c3e2a4fc758a5581b7
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-08-23 17:51:31 +0200

    update scripts

--- create_diamond_npz_training_oneat.py
+++ create_diamond_npz_training_oneat.py
@@ -24,7 +24,7 @@ from pathlib import Path
 #Specify the directory containing images
 image_dir = '/gpfsstore/rech/jsy/uzj81mi/Mari_Data_Training/oneat_training/oneat_train_raw/'
 #Specify the directory contaiing csv files
-csv_dir = '/gpfsstore/rech/jsy/uzj81mi/Mari_Data_Training/oneat_training/oneat_train_csv/'
+csv_dir = '/gpfsstore/rech/jsy/uzj81mi/Mari_Data_Training/oneat_training/oneat_train_diamond_csv/'
 #Specify the directory containing the segmentations
 seg_image_dir = '/gpfsstore/rech/jsy/uzj81mi/Mari_Data_Training/oneat_training/oneat_train_seg/'
 #Specify the model directory where we store the json of categories, training model and parameters
@@ -42,34 +42,34 @@ Path(save_dir).mkdir(exist_ok = True)
 event_type_name = ["Normal", "Division"]
 #Label corresponding to event
 event_type_label = [0, 1]
-
 #The name appended before the CSV files
 csv_name_diff = 'ONEAT'
 size_tminus = 1
 size_tplus = 1
 trainshapex = 64
 trainshapey = 64
+trainshapez = 16
 normalizeimage = True
 npz_name = 'Xenopus_oneat_training_m1p1_noise'
 npz_val_name = 'Xenopus_oneat_training_m1p1_noiseval'
-crop_size = [trainshapex,trainshapey,size_tminus,size_tplus]
+crop_size = [trainshapex,trainshapey,trainshapez,size_tminus,size_tplus]
 
 
-event_position_name = ["x", "y", "t", "h", "w", "c"]
-event_position_label = [0, 1, 2, 3, 4, 5]
+event_position_name = ["x", "y", "z", "t", "h", "w", "d", "c"]
+event_position_label = [0, 1, 2, 3, 4, 5, 6, 7]
 
 dynamic_config = TrainConfig(event_type_name, event_type_label, event_position_name, event_position_label)
 
 dynamic_json, dynamic_cord_json = dynamic_config.to_json()
 
-save_json(dynamic_json, model_dir + "Cellsplitcategoriesxenopus" + '.json')
+save_json(dynamic_json, model_dir + "Cellsplitdiamondcategoriesxenopus" + '.json')
 
-save_json(dynamic_cord_json, model_dir + "Cellsplitcordxenopus" + '.json')        
-
-
+save_json(dynamic_cord_json, model_dir + "Cellsplitdiamondcordxenopus" + '.json')        
 
 
-MovieCreator.MovieLabelDataSet(image_dir, 
+
+
+MovieCreator.VolumeLabelDataSet(image_dir, 
                                seg_image_dir, 
                                csv_dir, 
                                save_dir, 
@@ -82,7 +82,7 @@ MovieCreator.MovieLabelDataSet(image_dir,
 
 
 
-MovieCreator.createNPZ(save_dir, axes = 'STXYC', save_name = npz_name, save_name_val = npz_val_name)
+MovieCreator.createNPZ(save_dir, axes = 'STZXY', save_name = npz_name, save_name_val = npz_val_name)
 
 
 
