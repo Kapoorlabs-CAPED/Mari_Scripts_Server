f2677e8d9fd0a33a17e6246f23d6584d36942578
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-09-10 20:07:06 +0200

    clean headless

--- Mari_Scripts_OVH/Visualize_Results/visualize_oneat_napari_headless.py
+++ Mari_Scripts_OVH/Visualize_Results/visualize_oneat_napari_headless.py
@@ -3,22 +3,23 @@
 # coding: utf-8
 
 
-from slimoneat.NEATUtils import SLIMNEATViz
+from oneat.NEATUtils import NEATViz
 
 
 
 
 imagedir = '/mnt/WorkHorse/Mari_Data_Oneat/raw/'
 segimagedir = '/mnt/WorkHorse/Mari_Data_Oneat/seg/'
-csvdir = '/mnt/WorkHorse/Mari_Data_Oneat/oneat_results/'
+csvdir = '/mnt/WorkHorse/oneat_metrics/'
 categories_json = '/mnt/WorkHorse/Mari_Models/Oneat/Cellsplitcategoriesxenopus.json'
 fileextension = '*tif'
 start_project_mid = 4
 end_project_mid = 1
-Vizdetections = SLIMNEATViz(imagedir,
+Vizdetections = NEATViz(imagedir,
                         csvdir,
                         categories_json,
                         segimagedir = segimagedir,
+                        headless = True,
                         fileextension = fileextension,
                         start_project_mid = start_project_mid,
                         end_project_mid = end_project_mid)
