bed8c6fc5aefbb89b862b3a5af5877f45699775b
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-11-09 17:05:31 +0100

    ng jean zay scripts

--- pull_from_bucket.sh
+++ pull_from_bucket.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#!/bin/bash
 
-aws s3 cp s3://marioneat/Mari_Models/oneat_xenopus_volumetric_d29_f64/ /home/debian/WorkHorse/Mari_Models/Oneat/oneat_xenopus_volumetric/ --recursive
+aws s3 cp  s3://marioneat/Mari_Models/oneat_xenopus_resnet/ /gpfsstore/rech/jsy/uzj81mi/Mari_Models/Oneat/oneat_xenopus_resnet/ --recursive
+aws s3 cp s3://marioneat/Mari_Models/oneat_xenopus_densenet/ /gpfsstore/rech/jsy/uzj81mi/Mari_Models/Oneat/oneat_xenopus_densenet/  --recursive
+aws s3 cp s3://marioneat/Mari_Models/oneat_xenopus_sparsenet/ /gpfsstore/rech/jsy/uzj81mi/Mari_Models/Oneat/oneat_xenopus_sparsenet/  --recursive
